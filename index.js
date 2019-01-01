4a22b1948c173be921b315d5a6a56a439dcf0e58
Ngo Duy <duynq2197@gmail.com>
2019-01-02 06:46:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.set("view engine","ejs");
+app.set("views",__dirname + "/views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var arrayUserLogined = [""];
+
+io.on("connection",function(socket){
+	console.log("[CONNECTION] "+socket.id+ " connect to server");
+
+	//User register
+	socket.on("client-resgiter",function(data){
+		if(arrayUserLogined.indexOf(data) == -1){
+			//Register success
+			console.log("[RESGISTER] "+data + " resgister success");
+			arrayUserLogined.push(data);
+			socket.Username = data;
+			socket.emit("server-notice-register-success",socket.Username);
+			io.sockets.emit("server-request-update-list-people-online",arrayUserLogined);
+		}else{
+			//Register fail
+			console.log("[RESGISTER] "+data + " resgister fail");
+			socket.emit("server-notice-register-fail");
+		}
+	});
+
+	//User send message
+	socket.on("client-send-message-to-all",function(data){
+		io.sockets.emit("server-request-send-message-content",{username:socket.Username, content:data});
+	});
+
+	//User is typing
+	socket.on("i-am-typing",function(data){
+		if(data == "true"){
+			socket.broadcast.emit("someone-is-typing",socket.Username);
+		}else{
+			socket.broadcast.emit("someone-is-typing","");
+		}		
+	});
+
+	//User logout
+	socket.on("client-request-logout",function(){
+		//Delete array and update client
+		console.log("[LOGOUT] "+socket.Username+" logout ("+socket.id+")");
+		arrayUserLogined.splice(arrayUserLogined.indexOf(socket.Username),1);		
+		io.sockets.emit("server-request-update-list-people-online",arrayUserLogined);
+		
+	});
+
+	//Socket disconnect
+	socket.on("disconnect",function(){
+		console.log("[DISCONNECT] "+socket.Username+" disconnected ("+socket.id+")");
+	})
+});
+app.get("/",function(req,res){
+	res.render("trangchu");
+});
---
+++ public/xuly.js
@@ -0,0 +1,67 @@
+var socket = io("http://localhost:3000");
+$(document).ready(function(){
+	$("#login").show();
+	$("#chatroom").hide();
+
+	$("#btnRegister").click(function(){
+		socket.emit("client-resgiter",$("#inputRegisterContent").val());
+	});
+
+	$("#btnLogout").click(function(){
+		socket.emit("client-request-logout");
+		$("#login").show();
+		$("#chatroom").hide();
+	});
+
+	$("#inputUserMessage").focusin(function(){
+		socket.emit("i-am-typing","true");
+	});
+
+	$("#inputUserMessage").focusout(function(){
+		socket.emit("i-am-typing","false");
+	});
+
+	$("#btnSend").click(function(){
+		socket.emit("client-send-message-to-all",$("#inputUserMessage").val());
+	});
+
+	socket.on("server-notice-register-success",function(data){
+		$("#userActived").text(data);	
+		$("#login").hide();
+		$("#chatroom").show();			
+	});
+
+	socket.on("someone-is-typing",function(data){
+		if(data != ""){
+			//Co nguoi dang go chu
+			$("#notice-someone-is-typing").html(data+" is typing "+"<img id='img-typing' src='typing.gif'/>");
+		}else{
+			//Khong co ai go chu
+			$("#notice-someone-is-typing").html("");
+		}		
+	})
+
+	socket.on("server-request-update-list-people-online",function(data){
+		$("#tableUserLogined").html("");
+		for(var i= (data.length-1); i >= 0; i--){
+			if(i == data.length-10) break;
+			else if(i == data.length-1){				
+				$("#tableUserLogined").prepend("<tr><td>"+data[i]+"</tr></td><br>");
+			}
+			else{
+				$("#tableUserLogined").prepend("<tr><td>"+data[i]+"</tr></td>");
+			}
+		}
+	});
+
+	socket.on("server-request-send-message-content",function(data){
+		$("#chat-area-content").append("\n"+data.username+": "+data.content);
+		//roll to the newest
+		var textarea = document.getElementById('chat-area-content');
+		textarea.scrollTop = textarea.scrollHeight;
+	});
+
+	socket.on("server-notice-register-fail",function(data){
+		alert("Username is existed.");
+	});
+});

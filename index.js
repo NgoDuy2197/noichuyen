57106139ad69b4c0b476bd247a3b91a268732bda
NgoDuy2197 <46311410+NgoDuy2197@users.noreply.github.com>
2019-01-02 13:53:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set("views",__dirname + "/views");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 var arrayUserLogined = [""];
 
